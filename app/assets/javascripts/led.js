9e6fc0c5e09b33b6499419228715d6079523c32b
malprax <kingmalprax@gmail.com>
2015-06-01 17:28:33 +0800

    modified led index and add led.js

---
+++ app/assets/javascripts/led.js
@@ -0,0 +1,19 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+  $('#on').click(function(){
+    $.get('/on')
+  });
+  $('#off').click(function(){
+    $.get('/off')
+  });
+  var pusher = new Pusher('#{Pusher.key}');
+  var channel = pusher.subscribe('button');
+  channel.bind('down', function(data) {
+    $('body').css({background: 'red'})
+  });
+  channel.bind('up', function(data) {
+    $('body').css({background: 'white'})
+  });
