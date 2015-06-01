54e9421284c5c70a63e8af0ffdf7a20cb5c5370f
malprax <kingmalprax@gmail.com>
2015-06-01 17:35:16 +0800

    modified routes and led_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'led/index'
+  match '/on' => 'led#on'
+  match '/off' => 'led#off'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
