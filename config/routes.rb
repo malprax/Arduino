0f6dd565374bb6dd71743aea7336399e99dc7bbb
malprax <kingmalprax@gmail.com>
2015-06-01 16:51:33 +0800

    add led routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  match '/on' => 'led#on'
+  match '/off' => 'led#off'
 
+  root to: 'led#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
