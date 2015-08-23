9bfc1630ee635d9c8ae0823484f7702c62418a71
malprax <kingmalprax@gmail.com>
2015-08-24 06:37:51 +0800

    modify billings and members view

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,5 @@
 class Member < ActiveRecord::Base
-  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "Hanya Diperbolehkan Huruf Saja" }, length: { minimum: 3, message: "Nama Terlalu Pendek"  }
+  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: "Nama Depan/Panggilan Dengan Huruf Saja" }, length: { minimum: 3, message: "Nama Terlalu Pendek"  }
   validates :address, length: { minimum: 4, message: "Alamat Terlalu Pendek" }
   validates :phone, numericality: { only_integer: true, message: "Masukkan Hanya Angka Saja" }, length: { minimum: 10, message: "No Telepon Terlalu Pendek" }
 end
