6bd8ca50b6149c62987209a3a4e0a846551a96b8
malprax <kingmalprax@gmail.com>
2015-08-25 22:59:53 +0800

    edit pusher14

--- app/controllers/hello_world_controller.rb
+++
@@ -1,8 +0,0 @@
-# app/controllers/hello_world_controller.rb
-class HelloWorldController < ApplicationController
-  def hello_world
-    Pusher['test_channel'].trigger('my_event', {
-      message: 'hello world'
-    })
-  end
-end
--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,5 +1,5 @@
 # config/initializers/pusher.rb
-require 'pusher'
-
-Pusher.url = "https://429f0dfaaec2f4d689e8:808cd9f52d25422d41ec@api.pusherapp.com/apps/122739"
-Pusher.logger = Rails.logger
+# require 'pusher'
+#
+# Pusher.url = "https://429f0dfaaec2f4d689e8:808cd9f52d25422d41ec@api.pusherapp.com/apps/122739"
+# Pusher.logger = Rails.logger
