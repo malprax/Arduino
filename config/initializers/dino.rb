21a932645304cfb009104e693903b8f8864557d3
malprax <kingmalprax@gmail.com>
2015-08-26 16:53:39 +0800

    edit pusher37

--- config/initializers/dino.rb
+++ config/initializers/dino.rb
@@ -1,8 +1,8 @@
-pusher = YAML.load_file(File.join(Arduino::Application.root, 'config/pusher.yml'))
+# pusher = YAML.load_file(File.join(Arduino::Application.root, 'config/pusher.yml'))
 
-Pusher.app_id = pusher['app_id']
-Pusher.key = pusher['key']
-Pusher.secret = pusher['secret']
+Pusher.app_id = '122739'
+Pusher.key = '429f0dfaaec2f4d689e8'
+Pusher.secret = '808cd9f52d25422d41ec'
 
 
 
