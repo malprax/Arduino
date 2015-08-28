0d179e4388c94e2304c4b848741b73520005f8d1
malprax <kingmalprax@gmail.com>
2015-08-28 21:59:39 +0800

    fix error timezone

--- app/assets/javascripts/timezone.js
+++
@@ -1,7 +0,0 @@
-var timzone = $(function(){
-	var offset = - new Date().getTimezoneOffset()/60;
-	$.cookie("browser.tzoffset", offset, { expires: 30, path: '/' });
-});
-
-$( document ).ready(timezone);
-
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,9 @@
-class ApplicationController < ActionController::Base
-  
-                                                                                
+class ApplicationController < ActionController::Base                                         
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  around_filter :set_time_zone
-  
   def current_billings
     @current_billings = Billing.current
-  end
-  
-  private
-                                                                                 
-  def set_time_zone
-    return yield unless (utc_offset = cookies['browser.tzoffset']).present?
-      utc_offset = utc_offset.to_i
-      gmt_offset = if utc_offset == 0 then nil elsif utc_offset > 0 then -utc_offset else "+#{-utc_offset}" end
-      Time.use_zone("Etc/GMT#{gmt_offset}"){ yield }
-    rescue ArgumentError
-      yield
   end
   
   helper_method :current_billings
