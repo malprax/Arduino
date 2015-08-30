880f4930d3e4d47ab883bcdc24ec0f85007f8ad3
malprax <kingmalprax@gmail.com>
2015-08-30 12:06:36 +0800

    error pusher2

--- config/initializers/dino.rb
+++ config/initializers/dino.rb
@@ -1,8 +1,12 @@
 # pusher = YAML.load_file(File.join(Arduino::Application.root, 'config/pusher.yml'))
 
-Pusher.app_id = '122739'
-Pusher.key = '429f0dfaaec2f4d689e8'
-Pusher.secret = '808cd9f52d25422d41ec'
+# Pusher.app_id = '122739'
+# Pusher.key = '429f0dfaaec2f4d689e8'
+# Pusher.secret = '808cd9f52d25422d41ec'
+
+Pusher.app_id = '122740'
+    Pusher.key = '6a97661ed61a198a18cf'
+    Pusher.secret = 'bd808ec7f68f4ec890a2'
 
 
 
