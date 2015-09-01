2f21a8dfba7d00d6d65c91544b3c22f6500616d7
malprax <kingmalprax@gmail.com>
2015-09-02 05:54:34 +0800

    fix make rake2

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,7 +19,7 @@
 
 # Learn more: http://github.com/javan/whenever
 # every 1.day, :at => '00:00 am' do
-every 2.minutes do
+every 1.day, :at => '00:00 am' do
   rake "delete:remove_billings"
 end
 
