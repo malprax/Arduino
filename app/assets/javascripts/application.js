eb7103c2149759303ad90d659ac92395ed21b0d3
malprax <kingmalprax@gmail.com>
2015-09-17 13:03:19 +0800

    ready to heroku4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,38 +25,3 @@
 //= require_tree .
 	
 
-var main = $(function(){
-	$('#angkat_portal').click(function(){
-	   $.get('/angkat_portal')
-     $('body').css({background: 'red'})
-	 });
-
-	$('#turunkan_portal').click(function(){
-	   $.get('/turunkan_portal')
-     $('body').css({background: 'white'})
-	});
-  
-  
-   var pusher = new Pusher('429f0dfaaec2f4d689e8');
-   var channel = pusher.subscribe('button')
-   channel.bind('down', function(data) {
-    $('body').css({background: 'red'})
-   });
-   channel.bind('up', function(data) {
-    $('body').css({background: 'white'})
-   });
-	
-});
-
-$(document).ready(main);
-//
-//
-// function printpage()
-// {
-//    window.print()
-// }
-//
-// $(document).on('click', '#exclude', function(){
-// 	$('#report').find('.no_data').toggle();
-// });
-
--- app/assets/javascripts/led.js
+++ app/assets/javascripts/led.js
@@ -7,6 +7,15 @@ var led = $(function(){
      $.get('/turunkan_portal')
   });
   
+  var pusher = new Pusher('429f0dfaaec2f4d689e8');
+  var channel = pusher.subscribe('button')
+  channel.bind('down', function(data) {
+   $('body').css({background: 'red'})
+  });
+  channel.bind('up', function(data) {
+   $('body').css({background: 'white'})
+  });
+  
    // var pusher = new Pusher('#{Pusher.key}');
    // var channel = pusher.subscribe('button');
  //   channel.bind('angkat_portal', function(data) {
