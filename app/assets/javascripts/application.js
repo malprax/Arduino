6872a5934ed6d12cf2de0a80e8b88cc11aede6d6
malprax <kingmalprax@gmail.com>
2015-09-17 20:40:58 +0800

    modify js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,32 @@
 //= require message_flash
 //= require turbolinks
 //= require_tree .
+  
+  $(document).ready(function(){
+  	$('#angkat_portal').click(function(){
+  	   $.get('/angkat_portal')
+       $('body').css({background: 'red'})
+  	 });
+
+  	$('#turunkan_portal').click(function(){
+  	   $.get('/turunkan_portal')
+       $('body').css({background: 'white'})
+  	});
+
+    var pusher = new Pusher('6a97661ed61a198a18cf');
+    var channel = pusher.subscribe('button')
+    channel.bind('down', function(data) {
+     $('body').css({background: 'red'})
+    });
+    channel.bind('up', function(data) {
+     $('body').css({background: 'white'})
+    });
+ 
+
+  });
+
+
+
 	
   
  
