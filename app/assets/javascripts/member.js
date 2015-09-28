d9a8a9ddc064f6e28b4604fd3558d908a72a6a25
malprax <kingmalprax@gmail.com>
2015-09-28 22:37:00 +0800

    modify js

--- app/assets/javascripts/member.js
+++ app/assets/javascripts/member.js
@@ -1,3 +1,24 @@
 function printpage(){
   window.print()
 }
+
+// $(document).ready(function() {
+//     // get data every 1 seconds and refresh myDiv
+//     var pingAfter = 1000;
+//     var refreshId = setInterval(function() {
+//         window.reload('.member-index');
+//     }, pingAfter);
+// });
+
+$(document).ready(function () {
+    // will call refreshPartial every 3 seconds
+    setInterval(refreshPartial, 3000)
+
+});
+
+// calls action refreshing the partial
+function refreshPartial() {
+  $.ajax({
+    url: "members/"
+ })
+}
